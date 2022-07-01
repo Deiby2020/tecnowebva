f44e15400a00a700ae430cb9f8b8d864f1b5f85c
Deiby2020 <64172570+Deiby2020@users.noreply.github.com>
2022-07-01 16:37:04 -0400

    eliminar index

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-const app = express();
-const port = process.env.PORT || 5000;
-app.get('/', (req,res) => res.send('Bienvenido a mi api de graphql'));
-app.listen( port, () => console.log('El servidor esta trabjando en el pureto: ${port} '));
